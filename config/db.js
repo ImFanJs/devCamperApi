83ed425064f4dbbe3130bfc51b1f2a839b72629a
Imfanjs <imfanjs@gmail.com>
2021-07-17 17:07:55 -0600

    add mongodb connection and config

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+
+  // eslint-disable-next-line no-console
+  console.log(`MongoDB connected: ${conn.connection.host}`);
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 // Load routes
 const bootcamps = require('./routes/bootcamps');
@@ -15,10 +19,19 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
 app.use('/api/v1/bootcamps', bootcamps);
 
-app.listen(
+const server = app.listen(
   PORT,
   // eslint-disable-next-line no-console
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`),
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', error => {
+  // eslint-disable-next-line no-console
+  console.log('Unhandled Rejection reason:', error.message);
+  // close server && exit process
+  server.close(() => process.exit(1));
+});
